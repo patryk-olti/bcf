491a1d78a4c875b7cb1d2b1b4a6924fe0425240c
Patryk Oltuch <95oltuchpatryk@gmail.com>
2021-11-24 10:11:43 +0100

    added new font - roboto

--- src/Components/Span.js
+++ src/Components/Span.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import 'typeface-roboto';
+
 const Span = ({ children, fontSize, margin }) => {
 
     const styles = {
         margin: `${margin}`,
         fontSize: `${fontSize}px`,
-        letterSpacing: '2px'
+        letterSpacing: '2px',
+        fontFamily: 'roboto'
     }
 
     return(
